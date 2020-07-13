ee5ac293064c332b5130cbce05795734ca25556e
bighnesh0404 <saibighneshprusty@gmail.com>
2020-07-13 19:21:30 +0530

    Create New Task functions has been created
    Signed-off-by: bighnesh0404 <saibighneshprusty@gmail.com>

---
+++ public/scripts/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCDfjrFWC2r9tFTVTFF91qtjFOTCPEzqv0",
+    authDomain: "firestore-d4cf8.firebaseapp.com",
+    databaseURL: "https://firestore-d4cf8.firebaseio.com",
+    projectId: "firestore-d4cf8",
+    storageBucket: "firestore-d4cf8.appspot.com",
+    messagingSenderId: "751229841285",
+    appId: "1:751229841285:web:8f6cef0c65f9920eb49eec",
+    measurementId: "G-V3PKNY5CPF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- public/scripts/worktrollyUI.js
+++ public/scripts/worktrollyUI.js
@@ -14,3 +14,32 @@ $("#createNewTaskButton").click(function() {
 $("#backToMainFromCreateNewTask").click(function() {
     $('#createNewTask').hide(100);
 });
+
+$("#submitNewTask").click(function() {
+    // e.preventDefault();
+    var title = $("#titleCreateNewTask").val();
+    var des = $("#desCreateNewTask").val();
+    var priority = $("#priorityCreateNewTask").val();
+    var difficulty = $("#difficultyCreateNewTask").val();
+    var creator = $("#creatorCreateNewTask").val();
+    var assignee = $("#assigneeCreateNewTask").val();
+    var estimatedTime = $("#estimatedTimeCreateNewTask").val();
+    var status = $("#statusCreateNewTask").val();
+    var category = $("#categoryCreateNewTask").val();
+
+    console.log(title);
+    console.log(des);
+    console.log(priority);
+    console.log(difficulty);
+    console.log(creator);
+    console.log(assignee);
+    console.log(estimatedTime);
+    console.log(status);
+    console.log(category);
+
+    var createNewTask = firebase.functions().httpsCallable('createNewTask');
+    createNewTask({Title: title, Description: des, Priority: priority, Difficulty: difficulty, Creator: creator, Assignee: assignee, EstimatedTime: estimatedTime, Status: status,Category:category}).then(result=>{
+    console.log(result.data);
+    $("#createNewTask").hide(100);
+    });
+});
