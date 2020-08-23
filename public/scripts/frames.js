8402001544f8775b790d1c71eb693ef3b872d624
bighnesh0404 <saibighneshprusty@gmail.com>
2020-08-24 01:01:29 +0530

    Filtering the data
    Signed-off-by: bighnesh0404 <saibighneshprusty@gmail.com>

--- public/scripts/frames.js
+++ public/scripts/frames.js
@@ -8,6 +8,7 @@ function getTasks() {
 
 function getTasksData() {
     var result = "ok";
+
     document.getElementById("cardsList").innerHTML = "";
     firestore.collection("Bussiness")
         .onSnapshot(function(snapshot) {
@@ -62,61 +63,97 @@ function removeInstance(data) {
 
 function setDataIntoCard() {
 
-    document.getElementById("cardsList").innerHTML = "";
+    //document.getElementById("cardsList").innerHTML = "";
 
     dataset.forEach(dataObj => {
-        var title = dataObj.title;
-        var priority = dataObj.priority;
-        var assignee = dataObj.assignee;
-        var creator = dataObj.creator;
-
-        var tickets = document.getElementById("cardsList").innerHTML;
-
-        var frame = "<div class=\"card text-white bg-dark\" style=\"max-width: 32rem;\">";
-
-        frame += "<div class=\"card-header\"> XX/XX/XXXX </div>";
-        frame += "<div class=\"card-body py-3\">";
-        frame += "<div class=\"row pb-2\">";
-        frame += "<div class=\"col-md-4 col-4\">";
-        frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Id: </h6>";
-        frame += "</div>";
-        // frame += "<div class=\"col-md-8 col-8 px-2\">" + id + "</div>";
-        frame += "</div>";
-
-        frame += "<div class=\"row pb-2\">";
-        frame += "<div class=\"col-md-4 col-4\">";
-        frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Title: </h6>";
-        frame += "</div>";
-        frame += "<div class=\"col-md-8 col-8 px-2\">" + title + "</div>";
-        frame += "</div>";
-
-        frame += "<div class=\"row pb-2\">";
-        frame += "<div class=\"col-md-4 col-4\">";
-        frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Priority: </h6>";
-        frame += "</div>";
-        frame += "<div class=\"col-md-8 col-8 px-2\">" + priority + "</div>";
-        frame += "</div>";
-
-        frame += "<div class=\"row pb-2\">";
-        frame += "<div class=\"col-md-4 col-4\">";
-        frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Assignee: </h6>";
-        frame += "</div>";
-        frame += "<div class=\"col-md-8 col-8 px-2\">" + assignee + "</div>";
-        frame += "</div>";
-
-        frame += "<div class=\"row pb-2\">";
-        frame += "<div class=\"col-md-4 col-4\">";
-        frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Creator: </h6>";
-        frame += "</div>";
-        frame += "<div class=\"col-md-8 col-8 px-2\">" + creator + "</div>";
-        frame += "</div>";
-
-        frame += "</div>";
-        frame += "</div>";
-        tickets += frame;
-
-        document.getElementById("cardsList").innerHTML = tickets;
+        console.log(dataObj.status);
+        frames(dataObj);
     });
 
     return "ok";
+}
+
+function frames(dataObj)
+{
+    var title = dataObj.title;
+    var priority = dataObj.priority;
+    var assignee = dataObj.assignee;
+    var creator = dataObj.creator;
+
+    var tickets = document.getElementById("cardsList").innerHTML;
+    var frame = "<div class=\"card text-white bg-dark\" style=\"max-width: 32rem;\">";
+
+    frame += "<div class=\"card-header\"> XX/XX/XXXX </div>";
+    frame += "<div class=\"card-body py-3\">";
+    frame += "<div class=\"row pb-2\">";
+    frame += "<div class=\"col-md-4 col-4\">";
+    frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Id: </h6>";
+    frame += "</div>";
+    // frame += "<div class=\"col-md-8 col-8 px-2\">" + id + "</div>";
+    frame += "</div>";
+
+    frame += "<div class=\"row pb-2\">";
+    frame += "<div class=\"col-md-4 col-4\">";
+    frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Title: </h6>";
+    frame += "</div>";
+    frame += "<div class=\"col-md-8 col-8 px-2\">" + title + "</div>";
+    frame += "</div>";
+
+    frame += "<div class=\"row pb-2\">";
+    frame += "<div class=\"col-md-4 col-4\">";
+    frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Priority: </h6>";
+    frame += "</div>";
+    frame += "<div class=\"col-md-8 col-8 px-2\">" + priority + "</div>";
+    frame += "</div>";
+
+    frame += "<div class=\"row pb-2\">";
+    frame += "<div class=\"col-md-4 col-4\">";
+    frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Assignee: </h6>";
+    frame += "</div>";
+    frame += "<div class=\"col-md-8 col-8 px-2\">" + assignee + "</div>";
+    frame += "</div>";
+
+    frame += "<div class=\"row pb-2\">";
+    frame += "<div class=\"col-md-4 col-4\">";
+    frame += "<h6 class=\"card-title text-center py-1\" style=\"background-color: black;\"> Creator: </h6>";
+    frame += "</div>";
+    frame += "<div class=\"col-md-8 col-8 px-2\">" + creator + "</div>";
+    frame += "</div>";
+
+    frame += "</div>";
+    frame += "</div>";
+    frame += "<br>";
+    tickets += frame;
+
+    document.getElementById("cardsList").innerHTML = tickets;
+}
+
+function filteredData(option)
+{
+    dataset.forEach(dataObj => {
+        switch(option)
+        {
+            case "backlog":
+                if(dataObj.status != "Completed")
+                    frames(dataObj);
+                break;
+            
+            case "development":
+                if(dataObj.category == "Development")
+                    frames(dataObj);
+                break;
+                
+            case "development":
+                if(dataObj.category == "Bussiness")
+                    frames(dataObj);
+                break;
+                    
+            case "development":
+                if(dataObj.category == "Marketing")
+                    frames(dataObj);
+                break;
+
+            default: frames(dataObj);
+        }
+    });
 }
