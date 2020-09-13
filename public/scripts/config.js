03e7a0361c63f3796cf9e791acb93db996a6480e
Joelzac537 <joeltz2000@gmail.com>
2020-09-13 14:37:43 +0530

    feat(SprintNewPage): update configurationand status

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,23 +1,14 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
-//     authDomain: "worktrolly.firebaseapp.com",
-//     databaseURL: "https://worktrolly.firebaseio.com",
-//     projectId: "worktrolly",
-//     storageBucket: "worktrolly.appspot.com",
-//     messagingSenderId: "358996208239",
-//     appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
-//     measurementId: "G-FEQ8PFLLCV"
-// };
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCZ8WafqA_eMqHOto3FsQekahaX-aJfdaU",
-    authDomain: "sprinttest.firebaseapp.com",
-    databaseURL: "https://sprinttest.firebaseio.com",
-    projectId: "sprinttest",
-    storageBucket: "sprinttest.appspot.com",
-    messagingSenderId: "870867375123",
-    appId: "1:870867375123:web:1334eab52fddb99d8e5be6"
+var firebaseConfig = {
+    apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
+    authDomain: "worktrolly.firebaseapp.com",
+    databaseURL: "https://worktrolly.firebaseio.com",
+    projectId: "worktrolly",
+    storageBucket: "worktrolly.appspot.com",
+    messagingSenderId: "358996208239",
+    appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
+    measurementId: "G-FEQ8PFLLCV"
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
