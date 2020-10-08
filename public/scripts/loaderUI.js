039367c78fb1dfe912a2ad1f31b4008bd6956b86
shivamgoyal7 <goyalshivam661@gmail.com>
2020-10-08 22:03:10 +0530

    feat(backlog): fixed bug

--- public/scripts/loaderUI.js
+++ public/scripts/loaderUI.js
@@ -1,20 +1,17 @@
-
 function uiLoader() {
-	var deactivationResult = deactivateUI();
-	if(deactivationResult == "ok")
-	{
-		$("#"+newPage).show(100);
-		currentPage = newPage;
-		newPage = "";
-	}
+    var deactivationResult = deactivateUI();
+    if (deactivationResult == "ok") {
+        $("#" + newPage).show(100);
+        currentPage = newPage;
+        newPage = "";
+    }
 }
-function deactivateUI()
-{
-	var result = "error";
-	if(newPage != "")
-	{
-		$("#"+currentPage).hide(100);
-		result = "ok";
-	}
-	return result;
+
+function deactivateUI() {
+    var result = "error";
+    if (newPage != "") {
+        $("#" + currentPage).hide(100);
+        result = "ok";
+    }
+    return result;
 }
