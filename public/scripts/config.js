8ff24efa76600a31631f49035a0dcc49e88c73a0
Joelzac537 <joeltz2000@gmail.com>
2020-10-22 22:27:10 +0530

    feat(Description):updated changes

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,24 +1,12 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
-//     authDomain: "worktrolly.firebaseapp.com",
-//     databaseURL: "https://worktrolly.firebaseio.com",
-//     projectId: "worktrolly",
-//     storageBucket: "worktrolly.appspot.com",
-//     messagingSenderId: "358996208239",
-//     appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
-//     measurementId: "G-FEQ8PFLLCV"
-// };
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyArH3xQQaP4hXXRCuxRPSrNJ5CPE_lgXsY",
-    authDomain: "project-cf3a4.firebaseapp.com",
-    databaseURL: "https://project-cf3a4.firebaseio.com",
-    projectId: "project-cf3a4",
-    storageBucket: "project-cf3a4.appspot.com",
-    messagingSenderId: "244691770416",
-    appId: "1:244691770416:web:1376ad777dad4fff75a1f5",
-    measurementId: "G-Z8JNWCKCL0"
+var firebaseConfig = {
+    apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
+    authDomain: "worktrolly.firebaseapp.com",
+    databaseURL: "https://worktrolly.firebaseio.com",
+    projectId: "worktrolly",
+    storageBucket: "worktrolly.appspot.com",
+    messagingSenderId: "358996208239",
+    appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
+    measurementId: "G-FEQ8PFLLCV"
 };
 
 // Initialize Firebase
