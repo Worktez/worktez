bda4360bb318b28f9bc99c1fce6626f333a26ad0
shivamgoyal7 <goyalshivam661@gmail.com>
2020-11-23 21:39:33 +0530

    added sprint component

--- worktrolly-ui/src/app/body/create-new-sprint/create-new-sprint.component.ts
+++ worktrolly-ui/src/app/body/create-new-sprint/create-new-sprint.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, Input, ViewChild} from '@angular/core';
 import { NgForm }   from '@angular/forms';
 import { AngularFireFunctions } from '@angular/fire/functions';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Data } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { Main, MainDataId, RawDataId, RawDataType } from 'src/app/Interface/RawDataInterface';
 
 @Component({
   selector: 'app-create-new-sprint',
@@ -10,6 +15,7 @@ import { AngularFireFunctions } from '@angular/fire/functions';
 export class CreateNewSprintComponent implements OnInit {
 
   @ViewChild('form') form: NgForm;
+  @Input('currentSprintName') currentSprintName: string;
 
   startDate:string
   endDate:string
@@ -19,8 +25,40 @@ export class CreateNewSprintComponent implements OnInit {
   totalMarketing:string
 
 
-  constructor(private functions: AngularFireFunctions) { }
+  public rawData: Observable<RawDataId[]>;
+  public rawCollection: AngularFirestoreCollection<RawDataType>;
+
+  currentSprintNumber: number;
+
+  public mainData: Observable<MainDataId[]>;
+  public mainCollection: AngularFirestoreCollection<Main>;
+
+  constructor(private functions: AngularFireFunctions, private db: AngularFirestore) { }
+
   ngOnInit(): void {
+    this.rawCollection = this.db.collection<RawDataType>('RawData');
+    this.rawData = this.rawCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as RawDataType;
+        this.currentSprintNumber = data.CurrentSprintId + 1;
+        this.currentSprintName = "S"+this.currentSprintNumber;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+
+    this.readCurrentSprintData();
+  }
+
+  async readCurrentSprintData() {
+    this.mainCollection = this.db.collection<Main>('Main');
+    this.mainData = this.mainCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Main;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
   }
 
   async createNewSprint(){
