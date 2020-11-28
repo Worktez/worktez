1d51cb2d74f4bf5187e03c358f551ddde2e902ea
Arjun Dave <davearjun25@gmail.com>
2020-11-28 23:04:34 +0530

    Made changes to the colour scheme and also added dark theme

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,23 +1,13 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
-//     authDomain: "worktrolly.firebaseapp.com",
-//     databaseURL: "https://worktrolly.firebaseio.com",
-//     projectId: "worktrolly",
-//     storageBucket: "worktrolly.appspot.com",
-//     messagingSenderId: "358996208239",
-//     appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
-//     measurementId: "G-FEQ8PFLLCV"
-// };
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCE4UKde_kTwHaDTuQS5YubsCIbNqSNKJQ",
-    authDomain: "simplelogin-e5759.firebaseapp.com",
-    databaseURL: "https://simplelogin-e5759.firebaseio.com",
-    projectId: "simplelogin-e5759",
-    storageBucket: "simplelogin-e5759.appspot.com",
-    messagingSenderId: "462404006182",
-    appId: "1:462404006182:web:d2f78f33e56d4daedf51e7"
-  };
+var firebaseConfig = {
+    apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
+    authDomain: "worktrolly.firebaseapp.com",
+    databaseURL: "https://worktrolly.firebaseio.com",
+    projectId: "worktrolly",
+    storageBucket: "worktrolly.appspot.com",
+    messagingSenderId: "358996208239",
+    appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
+    measurementId: "G-FEQ8PFLLCV"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
