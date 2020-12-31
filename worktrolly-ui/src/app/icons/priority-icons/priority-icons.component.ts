4fb594b08de6fe97342676b879cf9dabe80b174c
Arjun Dave <davearjun25@gmail.com>
2020-12-31 22:26:14 +0530

    added icon files
    Signed-off-by: Arjun Dave <davearjun25@gmail.com>

---
+++ worktrolly-ui/src/app/icons/priority-icons/priority-icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-priority-icons',
+  templateUrl: './priority-icons.component.html',
+  styleUrls: ['./priority-icons.component.css']
+})
+export class PriorityIconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ worktrolly-ui/src/app/icons/status-icons/status-icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-icons',
+  templateUrl: './status-icons.component.html',
+  styleUrls: ['./status-icons.component.css']
+})
+export class StatusIconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
