714134c7ad66d8f217de7bd5ce69fe3fc922895f
Arjun Dave <davearjun25@gmail.com>
2021-01-04 20:27:42 +0530

    Added icons to the task cards
    Signed-off-by: Arjun Dave <davearjun25@gmail.com>

--- worktrolly-ui/src/app/icons/priority-icons/priority-icons.component.ts
+++ worktrolly-ui/src/app/icons/priority-icons/priority-icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-priority-icons',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriorityIconsComponent implements OnInit {
 
+  @Input('Priority') Priority : string
   constructor() { }
 
   ngOnInit(): void {
--- worktrolly-ui/src/app/icons/status-icons/status-icons.component.ts
+++ worktrolly-ui/src/app/icons/status-icons/status-icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-status-icons',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusIconsComponent implements OnInit {
 
+  @Input('Status') Status : string
   constructor() { }
 
   ngOnInit(): void {
