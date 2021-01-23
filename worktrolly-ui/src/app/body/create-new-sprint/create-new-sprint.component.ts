337e06f0790588cb3db7e676f0e9fa34b8d723eb
Joel Thomas Zachariah <joeltz2000@gmail.com>
2021-01-23 15:32:20 +0530

    feat(navbar):reverted changes

--- worktrolly-ui/src/app/body/create-new-sprint/create-new-sprint.component.ts
+++ worktrolly-ui/src/app/body/create-new-sprint/create-new-sprint.component.ts
@@ -114,7 +114,6 @@ export class CreateNewSprintComponent implements OnInit {
 
     try {
       const result = await callable({ StartDate: this.startDate, EndDate: this.endDate, TotalDevelopment: this.totalDevelopment, TotalBusiness: this.totalBusiness, TotalMarketing: this.totalMarketing, TotalOther: this.totalOther, Status: this.status, NewSprintId: this.currentSprintNumber }).toPromise();
-
       console.log("Successfully created a new sprint");
       console.log(result);
       this.router.navigate(['/']);
