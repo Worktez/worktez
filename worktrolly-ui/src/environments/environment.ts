086a2bb4f453432e1795a8701a27902a422c13f5
bighnesh0404 <saibighneshprusty@gmail.com>
2021-01-24 19:11:01 +0530

    feat(darkTheme)

--- worktrolly-ui/src/environments/environment.ts
+++ worktrolly-ui/src/environments/environment.ts
@@ -5,22 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    // apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
-    // authDomain: "worktrolly.firebaseapp.com",
-    // databaseURL: "https://worktrolly.firebaseio.com",
-    // projectId: "worktrolly",
-    // storageBucket: "worktrolly.appspot.com",
-    // messagingSenderId: "358996208239",
-    // appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
-    // measurementId: "G-FEQ8PFLLCV"
-    apiKey: "AIzaSyAaMjQnm3NnUmHHW2Smt6wJyRV7a0FIeFo",
-  authDomain: "wls1-b942d.firebaseapp.com",
-  databaseURL: "https://wls1-b942d.firebaseio.com",
-  projectId: "wls1-b942d",
-  storageBucket: "wls1-b942d.appspot.com",
-  messagingSenderId: "1056324548947",
-  appId: "1:1056324548947:web:642a1d3c3fab60460c49a6",
-  measurementId: "G-KWPDV4PSDG"
+    apiKey: "AIzaSyAP-cLcP8gB3bsUHSK9oexPq0BDykBPzzE",
+    authDomain: "worktrolly.firebaseapp.com",
+    databaseURL: "https://worktrolly.firebaseio.com",
+    projectId: "worktrolly",
+    storageBucket: "worktrolly.appspot.com",
+    messagingSenderId: "358996208239",
+    appId: "1:358996208239:web:73a1939b7267070d2dfb9a",
+    measurementId: "G-FEQ8PFLLCV"
   }
 };
 
