c86c421d0ec0e53c862d52a2b144ac7035946303
vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>
2021-02-13 21:12:52 +0530

    fix(release i0.5): fixes in release i0.5
    Signed-off-by: vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>

--- functions/patchAPI.js
+++ functions/patchAPI.js
@@ -1,31 +1,39 @@
-const functions = require('firebase-functions');
-var cors = require('cors')({ origin: true });
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-undef */
+/* eslint-disable require-jsdoc */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable max-len */
+// eslint-disable-next-line no-dupe-else-if
 
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+
+const admin = require("firebase-admin");
 
 const db = admin.firestore();
 
-exports.addCompletionDatePatch = functions.https.onRequest((request, response) => {
+exports.patch = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         console.log(request);
         const completionDate = request.body.data.CompletionDate;
         let id;
         let result;
-        let p1 = db.collection("Tasks").get().then((task) => {
+        const p1 = db.collection("Tasks").get().then((task) => {
                 task.forEach((doc) => {
                     id = doc.data().Id;
                     console.log(id);
                     db.collection("Tasks").doc(id).update({
-                            CompletionDate: completionDate
+                            CompletionDate: completionDate,
                         }).then(() => {
-                            result = { data: "It has worked!" }
+                            result = { data: "It has worked!" };
                             console.log("Patch succesfully completed!");
                             return response.status(200).send(result);
                         })
                         .catch(function(error) {
                             result = { data: error };
                             console.error("Patch error in writing creation date: ", error);
-                            return response.status(500).send(result)
+                            return response.status(500).send(result);
                         });
                 });
                 return Promise.resolve(p1);
@@ -33,7 +41,7 @@ exports.addCompletionDatePatch = functions.https.onRequest((request, response) =
             .catch(function(error) {
                 result = { data: error };
                 console.error("Patch error in getting tasks: ", error);
-                return response.status(500).send(result)
+                return response.status(500).send(result);
             });
     });
-})
+});
