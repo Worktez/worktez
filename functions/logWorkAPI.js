cfd680258dec820e80a7ddc47cd50fc9d39a6d38
shivamgoyal7 <goyalshivam661@gmail.com>
2021-04-19 23:06:43 +0530

    feat(activity): updated activity API and added activityTab in task description
    Signed-off-by: shivamgoyal7 <goyalshivam661@gmail.com>

--- functions/logWorkAPI.js
+++ functions/logWorkAPI.js
@@ -92,7 +92,7 @@ exports.logWork = functions.https.onRequest((request, response) => {
                 return Promise.resolve(updateSprintstatus);
             });
 
-            Activity.addActivity("LOGWORK_COMMENT", logWorkComment, taskId, date, time);
+            Activity.addActivity("LOGWORK_COMMENT", logWorkComment, taskId, date, time, documentID);
             const logWorkPromises = [promise1, promise2, promise3];
             Promise.all(logWorkPromises).then(() => {
                     result = { data: "Logged Work successfully!" };
