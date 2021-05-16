05954d26bdc5394be6f39f6bb3ee0253b0c40251
Anugraha Jawahar <anugrahajawahar@gmail.com>
2021-05-16 18:57:14 +0530

    collectionGroup removed

--- src/app/body/my-dashboard/performance-chart/performance-chart.component.ts
+++ src/app/body/my-dashboard/performance-chart/performance-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollectionGroup } from '@angular/fire/firestore';
+import { AngularFirestore} from '@angular/fire/firestore';
 import { Tasks} from 'src/app/Interface/TasksInterface';
 import { BackendService } from 'src/app/services/backend.service';
 import { ErrorHandlerService } from 'src/app/services/error-handler.service';
