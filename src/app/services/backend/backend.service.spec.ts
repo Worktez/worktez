5bfc33d0ac9a9d8bdd92bd7d1612b9d7232301b6
Murlidhar Pathak <47478882+murlidharpathakprogrammer@users.noreply.github.com>
2021-08-21 20:00:03 +0530

    Update backend.service.spec.ts

--- src/app/services/backend/backend.service.spec.ts
+++ src/app/services/backend/backend.service.spec.ts
@@ -1,15 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BackendService } from './backend.service';
 
 describe('BackendService', () => {
   let service: BackendService;
-
+  
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(BackendService);
   });
-
+  
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
