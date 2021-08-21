8b3823a5a3e401d4b6e60f443e8192e517154cee
Komal Sharma <52309262+k0malSharma@users.noreply.github.com>
2021-08-21 20:41:55 +0530

    Revert "feat (Automated log work input (status and works completion) and added userid"

--- src/app/services/backend/backend.service.spec.ts
+++ src/app/services/backend/backend.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+
 import { BackendService } from './backend.service';
 
 describe('BackendService', () => {
   let service: BackendService;
-  
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(BackendService);
