42139058cf0dd24f89c80fc40b12fc69e7c31411
Arpit kumar <87569269+ArpitKumar21@users.noreply.github.com>
2021-09-13 22:02:13 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   },
   useEmulators: true
 };
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
