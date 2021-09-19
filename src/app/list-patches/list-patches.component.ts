8fb90eb8b605b587c5076e60612b823a63d69199
Deepthi <deepthi.m.g.s@gmail.com>
2021-09-19 23:59:23 +0530

    List Patches Feature

---
+++ src/app/list-patches/list-patches.component.ts
@@ -0,0 +1,20 @@
+
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-list-patches',
+  templateUrl: './list-patches.component.html',
+  styleUrls: ['./list-patches.component.css']
+})
+export class ListPatchesComponent implements OnInit {
+
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore) { 
+    this.items = db.collection('Patches').valueChanges();
+  }
+
+  ngOnInit(): void {
+  }
+}
