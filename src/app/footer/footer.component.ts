454a376ee22b7a1614feadfdeb870d9df5391c6c
shivamgoyal7 <goyalshivam661@gmail.com>
2021-09-30 16:13:52 +0530

    feat(releaseApi): added github release data in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpServiceService } from 'src/app/services/http-service.service';
+import { GitData } from '../Interface/githubReleaseData';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  releaseData: string
+  tag_name: string
+  id: string
+  created_at: string
+  published_at: string
 
-  constructor() { }
+  constructor(private httpService: HttpServiceService) { }
 
   ngOnInit(): void {
+    try {
+      this.httpService.getReleaseDetails().pipe(map(data => {
+        const objData = data[0] as GitData;
+        return objData;
+      })).subscribe(data => {
+        this.tag_name = data.tag_name;
+        this.id = data.id;
+        this.created_at = data.created_at;
+        this.published_at = data.published_at;
+      });
+    } catch (error) {
+      
+    }
   }
 
 }
