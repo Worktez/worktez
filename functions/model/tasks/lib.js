713bd0042c73c51438c02f96887603a5ec19df3f
shibangi <70807786+shibangi23@users.noreply.github.com>
2021-10-02 19:40:53 +0530

    Updated the file lib.js.

--- functions/model/tasks/lib.js
+++ functions/model/tasks/lib.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 /* eslint-disable object-curly-spacing */
@@ -32,53 +31,6 @@ exports.setTask = function(orgDomain, taskId, title, des, priority, difficulty, 
         CompletionDate: completiondate,
         OrganizationId: orgId,
         TeamId: teamId,
-    });
-    return Promise.resolve(createTask);
-};
-
-exports.updateTask = function(inputJson, orgDomain, taskId) {
-    const updateTaskPromise = db.collection("Organizations").doc(orgDomain).collection("Tasks").doc(taskId).update(inputJson);
-    return Promise.resolve(updateTaskPromise);
-};
-
-exports.getTask = function(taskId, orgDomain) {
-    const getTaskDetails = db.collection("Organizations").doc(orgDomain).collection("Tasks").doc(taskId).get().then((taskDoc) => {
-        return taskDoc.data();
-    });
-    return Promise.resolve(getTaskDetails);
-
-/* eslint-disable linebreak-style */
-/* eslint-disable no-undef */
-/* eslint-disable object-curly-spacing */
-/* eslint-disable eol-last */
-/* eslint-disable indent */
-/* eslint-disable max-len */
-// const admin = require("firebase-admin");
-// const db = admin.firestore();
-
-const { db } = require("../application/lib");
-
-exports.setTask = function(orgDomain, taskId, title, des, priority, difficulty, creator, assignee, estimatedTime, status, project, loggedWorkTotalTime, workDone, sprintNumber, storyPointNumber, creationDate, completiondate, orgId, teamId, type) {
-    const createTask = db.collection("Organizations").doc(orgDomain).collection("Tasks").doc(taskId).set({
-        Id: taskId,
-        Title: title,
-        Description: des,
-        Priority: priority,
-        Difficulty: difficulty,
-        Creator: creator,
-        Assignee: assignee,
-        EstimatedTime: estimatedTime,
-        Status: status,
-        Project: project,
-        LogWorkTotalTime: loggedWorkTotalTime,
-        WorkDone: workDone,
-        SprintNumber: sprintNumber,
-        StoryPointNumber: storyPointNumber,
-        CreationDate: creationDate,
-        CompletionDate: completiondate,
-        OrganizationId: orgId,
-        TeamId: teamId,
-        Type: type,
     });
     return Promise.resolve(createTask);
 };
