ed664ad62fbfb6fd7f3784f7a07f015d4c2816f1
thanvi <thanvipendyala194@gmail.com>
2021-10-16 12:56:46 +0530

    added API filec

---
+++ functions/model/sprints/getSprintDetails.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable object-curly-spacing */
+const { getSprint } = require("./lib");
+const { createSprintName } = require("../application/lib");
+
+exports.getSprintDetails = function(request, response) {
+  const orgDomain = request.body.data.OrgDomain;
+  const sprintNumber = request.body.data.SprintNumber;
+  const teamName = request.body.data.TeamName;
+  const fullSprintName = createSprintName(sprintNumber);
+  console.log("check1", orgDomain);
+  console.log("check2", fullSprintName);
+  console.log("check3", teamName);
+  let result;
+  const status = 200;
+
+  const p1 = getSprint(orgDomain, teamName, fullSprintName).then((snapshot) => {
+    result = snapshot;
+  });
+
+  return Promise.resolve(p1).then(() => {
+    result = {data: {status: "Ok", sprintData: result}};
+    console.log(result);
+    return response.status(status).send(result);
+  }).catch((error) => {
+    console.error(error);
+    result= {data: {status: "Error", sprintData: undefined}};
+    return response.status(status).send(result);
+  });
+};
+
