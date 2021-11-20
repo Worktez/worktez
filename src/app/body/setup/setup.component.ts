3a9a807cc5a8270b5546d9fae0157e7120f2eb9b
vivek kumar <vvksindia@gmail.com>
2021-11-20 22:26:50 +0530

    fix(logs): fixed setup logs
    Signed-off-by: vivek kumar <vvksindia@gmail.com>

--- src/app/body/setup/setup.component.ts
+++ src/app/body/setup/setup.component.ts
@@ -191,7 +191,7 @@ export class SetupComponent implements OnInit {
     try {
       const result = await callable({ mode: "patchModerator", Patch: "Patch4", PatchName: "Patch-4", PatchDescription: "This patch allows the user to change a particular field in relevent tasks, enter field name and field value to get the task details", CreationDate: "07/07/2021", UpdatedOn: "12/08/2021", LastUsedByOrg: "", LastUsedByUid: ""}).toPromise();
 
-      console.log("Created Patch3 document");
+      console.log("Created Patch4 document");
       this.showLoader = false;
       console.log(result);
     } catch (error) {
@@ -201,7 +201,7 @@ export class SetupComponent implements OnInit {
     try {
       const result = await callable({ mode: "patchModerator", Patch: "Patch5", PatchName: "Patch-5", PatchDescription: "This patch allows the user to change a particular field in relevent tasks, enter field name and field value to get the task details", CreationDate: "07/07/2021", UpdatedOn: "12/08/2021", LastUsedByOrg: "", LastUsedByUid: ""}).toPromise();
 
-      console.log("Created Patch3 document");
+      console.log("Created Patch5 document");
       this.showLoader = false;
       console.log(result);
     } catch (error) {
@@ -211,7 +211,7 @@ export class SetupComponent implements OnInit {
     try {
       const result = await callable({ mode: "patchModerator", Patch: "Patch6", PatchName: "Patch-6", PatchDescription: "This patch allows the user to change a particular field in relevent tasks, enter field name and field value to get the task details", CreationDate: "07/07/2021", UpdatedOn: "12/08/2021", LastUsedByOrg: "", LastUsedByUid: ""}).toPromise();
 
-      console.log("Created Patch3 document");
+      console.log("Created Patch6 document");
       this.showLoader = false;
       console.log(result);
     } catch (error) {
