b7e194cbb27ca8b3234419d94fb3e0604bb104b5
printf-twinkle <twinklechokostar@gmail.com>
2021-12-25 00:54:23 +0530

    removed creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCrVt4B-tihzURNNSwqH8SEQUEvxrLPiV4",
-
-  authDomain: "dummy-fdfc5.firebaseapp.com",
-
-  projectId: "dummy-fdfc5",
-
-  storageBucket: "dummy-fdfc5.appspot.com",
-
-  messagingSenderId: "25127119651",
-
-  appId: "1:25127119651:web:052780386b82cfac517a96"
 
   },
   useEmulators: true,
