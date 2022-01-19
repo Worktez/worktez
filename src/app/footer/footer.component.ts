4afda27a2b0d2bda9137c56b1b1c84b61140bdbc
Abhishek Mishra <am1426620@gmail.com>
2022-01-19 21:55:17 +0530

    fix(release-notes-omponent): "done required changes "
    Signed-off-by: Abhishek Mishra <am1426620@gmail.com>

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -17,16 +17,16 @@ export class FooterComponent implements OnInit {
   constructor(public authService: AuthService, private httpService: HttpServiceService, public router: Router) { }
 
   ngOnInit(): void {
-    try {
-      this.httpService.getReleaseDetails().pipe(map(data => {
-        const objData = data[0] as GitData;
-        return objData;
-      })).subscribe(data => {
-        this.tag_name = data.tag_name;
+      try {
+        this.httpService.getReleaseDetails().pipe(map(data => {
+          const objData = data[0] as GitData;
+          return objData;
+        })).subscribe(data => {
+          this.tag_name = data.tag_name;
 
-      });
-    } catch (error) {      
-    }
+        });
+      } catch (error) {      
+      }
   }
   privacy(){
     this.router.navigate(['/privacy']);
