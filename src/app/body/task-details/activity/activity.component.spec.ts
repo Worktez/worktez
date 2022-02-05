fb007efba8c0b8ecc7e65d4a8ee745b5adcbd164
sukesh02 <sukeshraghav@gmail.com>
2022-02-05 21:32:54 +0530

    fix/AddCopyright

--- src/app/body/task-details/activity/activity.component.spec.ts
+++ src/app/body/task-details/activity/activity.component.spec.ts
@@ -1,3 +1,16 @@
+/*********************************************************** 
+* Copyright (C) 2022 
+* Worktez 
+* 
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the MIT License 
+* 
+* 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the MIT License for more details. 
+***********************************************************/
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActivityComponent } from './activity.component';
