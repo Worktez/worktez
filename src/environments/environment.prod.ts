734563c5ca737c851205e49196451e725d3803ef
vivek kumar <vvksindia@gmail.com>
2022-03-19 15:58:57 +0530

    feat(workflow): Added Github action for deployement
    Signed-off-by: vivek kumar <vvksindia@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    
+    authDomain: "worktrolly.firebaseapp.com",
+    databaseURL: "https://worktrolly.firebaseio.com",
+    projectId: "worktrolly",
+    storageBucket: "worktrolly.appspot.com",
+    apiKey: `${process.env.APIKEY}`,
+    appId: `${process.env.APPID}`,
+    measurementId: `${process.env.MEASUREMENTID}`,
+    messagingSenderId: `${process.env.MESSAGINGSENDERID}`,
   },
   useEmulators: false,
   gitApiUrl: "https://api.github.com/repos/Worktez/worktez",
