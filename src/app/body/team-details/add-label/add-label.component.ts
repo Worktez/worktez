b1fa4fd4d6a60c660741c7ea45c1b4071621c2d0
Simran Nigam <nigamsimran14@gmail.com>
2022-03-26 11:08:24 +0530

    fix(labels): added copyright

--- src/app/body/team-details/add-label/add-label.component.ts
+++ src/app/body/team-details/add-label/add-label.component.ts
@@ -1,3 +1,16 @@
+/*********************************************************** 
+* Copyright (C) 2022 
+* Worktez 
+* 
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the MIT License 
+* 
+* 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the MIT License for more details. 
+***********************************************************/
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { PopupHandlerService } from 'src/app/services/popup-handler/popup-handler.service';
 import { Label ,Team} from 'src/app/Interface/TeamInterface'
