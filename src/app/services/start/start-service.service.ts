f052f0e04c378fe74cb23b116eecdd4e08e6b88a
vivek kumar <vvksindia@gmail.com>
2022-04-25 12:31:09 +0530

    fix(console.log): Removed extra console.log
    Signed-off-by: vivek kumar <vvksindia@gmail.com>

--- src/app/services/start/start-service.service.ts
+++ src/app/services/start/start-service.service.ts
@@ -147,7 +147,6 @@ export class StartServiceService {
           this.applicationSettingsService.editedTeamId = this.selectedTeamId;
         }
         this.backendService.organizationsData.subscribe(data => {
-          console.log("p1");
           this.readApplicationData();
         });
         this.authService.myTeamsListObservable.subscribe(data => {
@@ -163,13 +162,10 @@ export class StartServiceService {
 
   readApplicationData() {
     this.showTeamsData = false;
-    console.log("check 1 false");
     this.applicationDataState.next(false);
     this.applicationSettingsService.team = undefined;
     this.applicationSettingsService.teamAvailable = false;
     this.applicationSettingsService.getTeamDetails(this.selectedTeamId).subscribe(teams => {
-      console.log("p2");
-      console.log(teams);
       this.teamData = teams;
       if (this.teamData.TeamId == this.selectedTeamId) {
         if (this.applicationSettingsService.editedSprintId != this.teamData.CurrentSprintId && this.changeTeam == false && this.applicationSettingsService.editedSprintId != 0 ) {
@@ -191,7 +187,6 @@ export class StartServiceService {
         this.role = "Member";
       }
       this.showTeamsData = true;
-      console.log("p3 : true");
       this.applicationDataState.next(true);
       return this.teamData;
     });
