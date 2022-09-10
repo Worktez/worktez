d8f205cfecf181060e9b8c789f984490287d54cb
sanjaykrishna1203 <sanjaykrishna1203@gmail.com>
2022-09-10 20:16:13 +0530

    fix(milestone):Removed Unnecessary files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDpJwcl3iwUUuoGmt-hHUN5nI1wiyE5yLw",
-    authDomain: "worktez-1203.firebaseapp.com",
-    databaseURL: "https://worktez-1203-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "worktez-1203",
-    storageBucket: "worktez-1203.appspot.com",
-    messagingSenderId: "552074837362",
-    appId: "1:552074837362:web:c9da08e7327ea70f80f1eb",
-    measurementId: "G-HVWN409YZV"
+    
   },
   useEmulators: true,
   gitApiUrl: "https://api.github.com/repos/Worktez/worktez",
