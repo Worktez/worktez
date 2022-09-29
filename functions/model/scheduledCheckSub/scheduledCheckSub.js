8dc4de2fd78acf3013805e7735132fba4a2d67b5
Aditya Khedekar <aditya3034@gmail.com>
2022-09-29 23:51:39 +0530

    Added Schedular For Subscriptions

--- functions/model/scheduledCheckSub/scheduledCheckSub.js
+++ functions/model/scheduledCheckSub/scheduledCheckSub.js
@@ -8,7 +8,7 @@
 /** *********************************************************
  * Copyright (C) 2022
  * Worktez
- *
+ * Author: Aditya Khedekar <aditya3034@gmail.com>
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the MIT License
  *
--- functions/model/subscriptions/tark/checkSubscription.js
+++ functions/model/subscriptions/tark/checkSubscription.js
@@ -1,3 +1,16 @@
+/*********************************************************** 
+* Copyright (C) 2022 
+* Worktez 
+* Author: Aditya Khedekar <aditya3034@gmail.com>
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the MIT License 
+* 
+* 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+* See the MIT License for more details. 
+***********************************************************/
 const { basicSubscription, currentDate, standardSubscription } = require("../../application/lib");
 const { getSubscriptions, updateSubscription } = require("../lib");
 
