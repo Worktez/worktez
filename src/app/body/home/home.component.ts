78d6bdfeda84c7f36099ed617eba06934734fc5d
Thanvi <thanvipendyala194@gmail.com>
2022-10-02 20:57:38 +0530

    fix: Fixed testimonial pictures

--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -29,19 +29,19 @@ export class HomeComponent implements OnInit {
 
   testimonials = [
     {
-      Image: "https://media-exp1.licdn.com/dms/image/C5603AQGkf_jPWudDdA/profile-displayphoto-shrink_400_400/0/1629381452691?e=1664409600&v=beta&t=5LSJ9aQLJMFtd6UPOuwH6_63ZXKc1JYS-E9oZO8WBpI",
+      Image: "https://media-exp1.licdn.com/dms/image/C5603AQGkf_jPWudDdA/profile-displayphoto-shrink_800_800/0/1629381452691?e=1670457600&v=beta&t=0XZJxw8qq_KHtr25vicQgXxvIdGTPn6yNbkAPMb2JFI",
       Name: "Prashant Pandey",
       Position: "C.E.O Global Child Prodigy",
       Content: "Worktez has revolutionised the complex DevOps operations for our project. It is helping us understand our team and individual strength."
     },
     {
-      Image: "https://media-exp1.licdn.com/dms/image/C4D03AQFawTO3RRB3Yw/profile-displayphoto-shrink_400_400/0/1656944446639?e=1664409600&v=beta&t=C7Jx3g6f8zf_leYrSXNg9quyI8k1OD5CB17sxde6LBU",
+      Image: "https://media-exp1.licdn.com/dms/image/C4D03AQFawTO3RRB3Yw/profile-displayphoto-shrink_800_800/0/1656944446639?e=1670457600&v=beta&t=oalQ3y1n0wL4sN6us6GZH9amLbCy8fNuvtiQbN7X_SY",
       Name: "Praveen Das",
       Position: "Senior Regional Partnerships Manager, South Asia at Wikimedia",
       Content: "Worktez is a great product designed to add value to your team and project. The milestone-based scrum innovation helps the project to be on time with better quality."
     },
     {
-      Image: "https://media-exp1.licdn.com/dms/image/C5603AQHpP3ccXAJPPw/profile-displayphoto-shrink_400_400/0/1516340117179?e=1664409600&v=beta&t=zEe3rU3MuWHOcC0MM4MMp0Q8RoYTiJxrxXmhZi4uOgI",
+      Image: "https://media-exp1.licdn.com/dms/image/C5603AQHpP3ccXAJPPw/profile-displayphoto-shrink_800_800/0/1516340117179?e=1670457600&v=beta&t=OzSm_AR7vYWnP6kVQ1OKWPVFCm26h1hvmyjqq_wsycg",
       Name: "Jitendra Singh",
       Position: "Product Manager at Hewlett Packard Inc.",
       Content: "The simplified model of the scrum with an automated approach helps focus on dedicated innovation and quality."
