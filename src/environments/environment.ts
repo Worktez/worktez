1b38d4dcf8fba4e9369ed602c8343578f8838ac0
Anurag Patil <anura@DESKTOP-UGFPG9F.(none)>
2022-10-05 21:59:27 +0530

    Fix/kanbanScrollBarUi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    
+    apiKey: "AIzaSyA74585UJSAlOLjeMw5S1PGVB_LYCG7WWA",
+authDomain: "worktez--first.firebaseapp.com",
+projectId: "worktez--first",
+storageBucket: "worktez--first.appspot.com",
+messagingSenderId: "410141146214",
+appId: "1:410141146214:web:bf0e8b99f175fc084341a7"
   },
   useEmulators: true,
   gitApiUrl: "https://api.github.com/repos/Worktez/worktez",
