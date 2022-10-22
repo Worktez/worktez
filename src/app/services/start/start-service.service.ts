54afda4afcb7131b0e4f41571e1e456147ffb0ba
Simran Nigam <nigamsimran14@gmail.com>
2022-10-23 00:45:35 +0530

    fix/releaseBugs2

--- src/app/services/start/start-service.service.ts
+++ src/app/services/start/start-service.service.ts
@@ -21,8 +21,8 @@ import { ApplicationSettingsService } from '../applicationSettings/application-s
 import { AuthService } from '../auth/auth.service';
 import { BackendService } from '../backend/backend.service';
 import { QuickNotesService } from '../quickNotes/quick-notes.service';
-import { SubscriptionService } from '../subscription/subscription.service';
 import { RBAService } from '../RBA/rba.service';
+import { SubscriptionService } from '../subscription/subscription.service';
 
 @Injectable({
   providedIn: 'root'
