63c50aebadaed317e7ec0289c07ff749aa517b7f
Simran Nigam <nigamsimran14@gmail.com>
2022-10-23 01:05:31 +0530

    lint issues

--- functions/model/application/paymentKeys.js
+++ functions/model/application/paymentKeys.js
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
 exports.keyId = "rzp_test_nfhDfN6X5cgp42";
 exports.keySecret = "EjWL1pPedHeT4Z1C4laM3u1b";
 exports.orderKey = "rzp_test_nfhDfN6X5cgp42";
