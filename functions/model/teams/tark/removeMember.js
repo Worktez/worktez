d0de22241caebbc6a8689b35a076a33af96ada5f
Swapnil Bankar <swapnilbankar1010@gmail.com>
2022-11-05 18:01:57 +0530

    Decrement Member counter on Member remove

--- functions/model/teams/tark/removeMember.js
+++ functions/model/teams/tark/removeMember.js
@@ -21,16 +21,17 @@
 /* eslint-disable max-len */
 // eslint-disable-next-line no-dupe-else-if
 
-const { updateTeamDetails } = require("../lib");
-
+const { updateTeamDetails, getAllTeams} = require("../lib");
+const { getOrgRawData, updateOrgRawData } = require("../../organization/lib");
 exports.removeMember = function(request, response) {
     const orgDomain = request.body.data.OrganizationDomain;
     const teamName = request.body.data.TeamName;
     const teamMembers = request.body.data.TeamMembers;
     const remove = request.body.data.Remove;
+    let flag = false;
     let result;
     let status = 200;
-
+    const res2 = {};
     const index = teamMembers.indexOf(remove);
     if (index != -1) {
         teamMembers.splice(index, 1);
@@ -38,6 +39,29 @@ exports.removeMember = function(request, response) {
             TeamMembers: teamMembers,
         };
         updateTeamDetails(updateJson, orgDomain, teamName);
+        getAllTeams(orgDomain).then((docs) => {
+            docs.forEach((element) => {
+                const teamdata = element.data();
+                const teamMembers = teamdata.TeamMembers;
+                res2[teamMembers]={};
+                    if (teamMembers.includes(remove)) {
+                        flag=true;
+                    }
+            });
+        }).catch((error) => {
+            status = 500;
+            console.log("Error: ", error);
+        });
+        if (!flag) {
+            getOrgRawData(orgDomain).then((rawData) => {
+                let totalNumberOfMembers = parseInt(rawData.TotalNumberOfMembers);
+                totalNumberOfMembers =totalNumberOfMembers- 1;
+                const updateRawDataInputJson = {
+                    TotalNumberOfMembers: totalNumberOfMembers,
+                };
+                updateOrgRawData(updateRawDataInputJson, orgDomain);
+            });
+        }
         result = { data: "Member removed Successfully" };
         console.log("Member removed Successfully");
     } else {
