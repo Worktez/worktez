3961327c371a275c5e1a2be03d8ab2f965558135
Simran Nigam <95395832+simran142002@users.noreply.github.com>
2022-11-15 17:49:22 +0530

    fixed milestone status edit (#824)
    * fixed milestone status edit
    
    * removed unnecessary changes
    
    * made the required changes
    
    * made the changes

--- src/app/body/milestone-details/milestone-details.component.ts
+++ src/app/body/milestone-details/milestone-details.component.ts
@@ -155,6 +155,8 @@ export class MilestoneDetailsComponent implements OnInit {
               return element;
             }
           });
+          this.milestoneIdToEdit =  "";
+          this.showLoader = false;
         },
         error: (error) => {
           console.log(error);
@@ -166,6 +168,54 @@ export class MilestoneDetailsComponent implements OnInit {
         }
       });
   }
+
+  clickOut() {
+    this.milestoneIdToEdit="";
+    this.submit();
+  }
+
+  submit() {
+    let data = [{ label: "milestoneStatus", value: this.milestoneData.MilestoneStatus }];
+
+      this.newVal = [this.milestoneData.MilestoneStatus];
+      this.generateChanges();
+      console.log("Inputs are valid");
+      this.editStatus();
+      this.showLoader=true;
+      this.getMilestoneDetails();
+
+  }
+
+  editStatus(){
+    this.showLoader=true;
+    const orgDomain = this.backendService.getOrganizationDomain();
+    console.log(orgDomain, this.milestoneId, this.milestoneData.MilestoneStatus, this.milestoneData.Title, this.milestoneData.Description, this.milestoneData.CreationDate,this.milestoneData.EndDate);
+    const callable = this.functions.httpsCallable('milestone/editMilestone');
+    callable({MilestoneId:this.milestoneId, OrgDomain:orgDomain,MilestoneStatus:this.milestoneData.MilestoneStatus, Title: this.milestoneData.Title, Description: this.milestoneData.Description, StartDate: this.milestoneData.CreationDate, EndDate: this.milestoneData.EndDate}).subscribe({
+      next: (data) => {
+        this.milestoneData.MilestoneStatus=this.milestoneData.MilestoneStatus;
+          this.showLoader=true;
+          this.milestoneIdToEdit =  "";
+          this.showLoader = false;
+      },
+      error: (error) => {
+        this.errorHandlerService.showError = true;
+        this.errorHandlerService.getErrorCode(this.componentName,"InternalError","Api");
+        this.showLoader = false;
+        console.error(error);
+      },
+      complete: () => console.info('Successful')
+    });
+  }
+
+  generateChanges() {
+    if (this.prevVal[0] != this.newVal[0])
+      this.changedData = this.changedData + " milestoneStatus,";
+    if (this.changedData != "")
+      this.changedData = "Edited-" + this.changedData;
+    this.changedData = this.changedData.substring(0, this.changedData.length - 1) + "."
+  }
+
 
 
   setMilestoneWidth = function () {
@@ -184,7 +234,7 @@ export class MilestoneDetailsComponent implements OnInit {
     this.applicationSetting.getTeamDetails(teamId);
     this.showLoader =false;
     const team = this.applicationSetting.team;
-    this.milestoneStatusLabels = team.MilestoneStatus;
+    this.milestoneStatusLabels = team.MilestoneStatus;    
   }
  
 
