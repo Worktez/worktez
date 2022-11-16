fa57df4f40656abeb50617038874bd03b7538356
Simran Nigam <nigamsimran14@gmail.com>
2022-11-16 22:05:13 +0530

    added copyright

--- functions/model/socialPage/tark/getAllSocialPageData.js
+++ functions/model/socialPage/tark/getAllSocialPageData.js
@@ -1,3 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable eol-last */
+
+/** *********************************************************
+ * Copyright (C) 2022
+ * Worktez
+ * Author: Simran Nigam <nigamsimran14@gmail.com>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the MIT License
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the MIT License for more details.
+ ***********************************************************/
 const {getAllPosts, getCommentsContent} = require("../lib");
 const {getReactionsContent} = require("../lib");
 
