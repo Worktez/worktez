a5c69ebfb425b658a03e966fb95dca287e8e2786
Swapnil Bankar <swapnilbankar1010@gmail.com>
2023-02-11 17:09:20 +0530

    add author name

--- src/app/body/team-details/git-integration/git-integration.component.ts
+++ src/app/body/team-details/git-integration/git-integration.component.ts
@@ -1,16 +1,16 @@
 /*********************************************************** 
 * Copyright (C) 2022 
 * Worktez 
-* 
+* Author : Swapnil Bankar <swapnilbankar1010@gmail.com>
+*    
 * This program is free software; you can redistribute it and/or 
 * modify it under the terms of the MIT License 
-* 
 * 
 * This program is distributed in the hope that it will be useful, 
 * but WITHOUT ANY WARRANTY; without even the implied warranty of 
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 * See the MIT License for more details. 
-***********************************************************/
+***********************************************************/ 
 import { Component, Input, OnInit } from '@angular/core';
 import { Team } from 'src/app/Interface/TeamInterface';
 import { BackendService } from 'src/app/services/backend/backend.service';
--- src/app/services/gitCDM-service/git-cdm-service.service.ts
+++ src/app/services/gitCDM-service/git-cdm-service.service.ts
@@ -1,16 +1,16 @@
 /*********************************************************** 
 * Copyright (C) 2022 
 * Worktez 
-* 
+* Author : Swapnil Bankar <swapnilbankar1010@gmail.com>
+*    
 * This program is free software; you can redistribute it and/or 
 * modify it under the terms of the MIT License 
-* 
 * 
 * This program is distributed in the hope that it will be useful, 
 * but WITHOUT ANY WARRANTY; without even the implied warranty of 
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 * See the MIT License for more details. 
-***********************************************************/
+***********************************************************/ 
 import { Injectable } from '@angular/core';
 import { AngularFireFunctions } from '@angular/fire/compat/functions';
 import { TeamServiceService } from '../team/team-service.service';
