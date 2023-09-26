7b3fb9e647e3965db811bd7947904698367feac7
abhishek-mishra019 <am1426620@gmail.com>
2023-09-26 21:16:53 +0530

    additional commit

--- functions/model/sprints/tark/updateAutoSprintStatus.js
+++ functions/model/sprints/tark/updateAutoSprintStatus.js
@@ -18,6 +18,7 @@
  ***********************************************************/
 
 const { currentDate, currentTime, createSprintName, addDays } = require("../../application/lib");
+const { sprintCreationMailer } = require("../../mailer/lib");
 const { getOrgUseAppKey } = require("../../organization/lib");
 const { migrateTasks } = require("../../tasks/tark/migrateTasks");
 const { getTeamUseTeamId, updateTeamDetails } = require("../../teams/lib");
@@ -114,6 +115,7 @@ exports.updateAutoSprintStatus = function(appKey, teamId) {
       };
       const updateTeamCurrentSprint = updateTeamDetails(inputJson, orgDomain, teamName);
 
+      sprintCreationMailer(orgDomain, teamName, createSprintName(newSprintNumber), startDate);
       const promises = [getSprintPromise, createSprint, updateTeamCurrentSprint];
       return Promise.all(promises);
     }).catch((error) => {
@@ -127,7 +129,6 @@ exports.updateAutoSprintStatus = function(appKey, teamId) {
     result = { data: "Sprint Updated Successfully" };
     console.log("Sprint final Updated Successfully");
     migrateTasks(orgDomain, teamId, teamName, oldSprintNumber, newSprintNumber, orgId);
-    // mailer for creating new sprint.
     return result;
   }).catch((error) => {
     result = { data: error };
